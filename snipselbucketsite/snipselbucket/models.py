f2b0625ba4d88f8fb9efc8e34e47c541fb014505
Mkpb321 <mike.burkhard@hotmail.com>
2022-04-26 15:12:41 +0200

    added timestamps

--- snipselbucketsite/snipselbucket/models.py
+++ snipselbucketsite/snipselbucket/models.py
@@ -5,6 +5,8 @@ class Snipsel(models.Model):
     text = models.TextField()
     source = models.TextField()
     weight = models.IntegerField(choices=list(zip(range(1, 101), range(1, 101))))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
         
     def __str__(self):
         return self.text[:10] + "... (" + self.source + ")"
@@ -15,6 +17,8 @@ class Snipsel(models.Model):
 class Comment(models.Model):
     text = models.TextField()
     snipsel = models.ForeignKey(Snipsel, related_name="comments", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
         
     def __str__(self):
         return self.text[:10] + "... (" + self.snipsel.text[:10] + ")"
