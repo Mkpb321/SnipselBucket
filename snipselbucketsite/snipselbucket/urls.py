4b48f28379d7f3a7c4e0e27f1d022d9389fe9bf5
Mkpb321 <mike.burkhard@hotmail.com>
2022-04-26 15:01:36 +0200

    added comment CRUDL

--- snipselbucketsite/snipselbucket/urls.py
+++ snipselbucketsite/snipselbucket/urls.py
@@ -1,18 +1,22 @@
 from django.urls import path
 from . import views
-from .views import SnipselList
-from .views import SnipselCreate
-from .views import SnipselDetail
-from .views import SnipselUpdate
-from .views import SnipselDelete
+from .views import SnipselList, SnipselCreate, SnipselDetail, SnipselUpdate, SnipselDelete
+from .views import CommentList, CommentCreate, CommentDetail, CommentUpdate, CommentDelete
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('snipsel/', SnipselList.as_view(), name='snipsel_list'),
-    path('new/', SnipselCreate.as_view(), name='snipsel_create'),
-    path('<int:pk>/', SnipselDetail.as_view(), name='snipsel_detail'),
-    path('<int:pk>/update/', SnipselUpdate.as_view(), name='snipsel_update'),
-    path('<int:pk>/delete/', SnipselDelete.as_view(), name='snipsel_delete'),
+    path('snipsel/new/', SnipselCreate.as_view(), name='snipsel_create'),
+    path('snipsel/<int:pk>/', SnipselDetail.as_view(), name='snipsel_detail'),
+    path('snipsel/<int:pk>/update/', SnipselUpdate.as_view(), name='snipsel_update'),
+    path('snipsel/<int:pk>/delete/', SnipselDelete.as_view(), name='snipsel_delete'),
+    
+    path('comment/', CommentList.as_view(), name='comment_list'),
+    path('comment/new/', CommentCreate.as_view(), name='comment_create'),
+    path('comment/<int:pk>/', CommentDetail.as_view(), name='comment_detail'),
+    path('comment/<int:pk>/update/', CommentUpdate.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDelete.as_view(), name='comment_delete'),
 ]
 
 # from django.urls import path
--- snipselbucketsite/snipselbucket/views.py
+++ snipselbucketsite/snipselbucket/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.views.generic import ListView
-from .models import Snipsel
+from .models import Snipsel, Comment
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 import os
 
 def index(request):
     return render(request, 'snipselbucket/index.html')
+
+
+## Snipsel ##
 
 class SnipselList(ListView):
     model = Snipsel
@@ -33,6 +36,33 @@ class SnipselDelete(DeleteView):
     model = Snipsel
     template_name = 'snipselbucket/snipsel_delete.html'
     success_url = reverse_lazy('snipsel_list')
+
+
+## Comment ##
+
+class CommentList(ListView):
+    model = Comment
+    template_name = 'snipselbucket/comment_list.html'
+
+class CommentCreate(CreateView):
+    model = Comment
+    template_name = 'snipselbucket/comment_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('comment_list')
+
+class CommentDetail(DetailView):
+    model = Comment
+    template_name = 'snipselbucket/comment_detail.html'
+
+class CommentUpdate(UpdateView):
+    model = Comment
+    template_name = 'snipselbucket/comment_update.html'
+    fields = '__all__'
+
+class CommentDelete(DeleteView):
+    model = Comment
+    template_name = 'snipselbucket/comment_delete.html'
+    success_url = reverse_lazy('comment_list')
 
 # def helloWorld(request):
 #     return HttpResponse(os.name)
